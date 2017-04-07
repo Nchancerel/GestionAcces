6bcd7fdc37ffeb3c3564be90a9c8458a33d312ef
Nicolas Chancerel <nicolas.chancerel@stfelixlasalle.fr>
2017-04-07 10:52:58 +0200

    f

---
+++ AfficheurTagRFID/AfficheurTagRFID/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Windows;
+using MahApps.Metro.Controls;
+using ModbusTCP;
+using System.Windows.Threading;
+using System.Threading;
+
+namespace AfficheurTagRFID
+{
+    public partial class MainWindow : MetroWindow
+    {
+        private ModbusTCP.Master MBmaster;
+        public delegate void D_UpdateUI_tagRFID(byte[] values);
+        public delegate void D_UpdateUI_empty();
+
+        //===============================================================
+        #region MAIN
+        public MainWindow()
+        {
+            InitializeComponent();
+            //===============================================================
+            #region APELLE DE LA METHODE CONNECTION A LA STATION
+            RFID_Connect();
+            #endregion
+
+            //===============================================================
+            #region CREATION ET LANCEMENT DU THREAD DE LECTURE RFID
+            Thread thread_ReadRFID = new Thread(ReadRFID);
+            thread_ReadRFID.Start();
+            #endregion
+        }
+        #endregion
+
+        //===================================================================
+        #region ACTION CLICK BOUTON SETTINGS
+        private void openSettings(object sender, RoutedEventArgs e)
+        {
+            Settings Popup_settings = new Settings();
+            Popup_settings.ShowDialog();
+        }
+        #endregion
+
+        //===================================================================
+        #region CONNECTION A LA STATION RFID
+        private void RFID_Connect()
+        {
+            try
+            // Create new modbus master and add event functions
+            {
+                MBmaster = new Master((string)Properties.Settings.Default["adresseIP"], 502);
+                MBmaster.OnResponseData += new ModbusTCP.Master.ResponseData(MBmaster_OnResponseData);
+                MBmaster.OnException += new ModbusTCP.Master.ExceptionData(MBmaster_OnException);
+                MessageBox.Show("Connexion a la station RFID réussie","Connexion réussie",MessageBoxButton.OK);
+            }
+            catch { MessageBox.Show("Connexion a la station RFID échouée", "Connexion échouée", MessageBoxButton.OK); }
+        }
+        #endregion
+
+        //===================================================================
+        #region THREAD LECTURE DE LA STATION RFID
+        private void ReadRFID()
+        {
+            while (true) {
+                ushort ID = ushort.Parse("3");
+                byte unit = Convert.ToByte(Properties.Settings.Default.Unit);
+                ushort StartAddress = ushort.Parse(Properties.Settings.Default.startAddress);
+                byte Length = Convert.ToByte(Properties.Settings.Default.Size);
+
+                MBmaster.ReadHoldingRegister(ID, unit, StartAddress, Length);
+                Thread.Sleep(50);
+            }
+
+        }
+        #endregion
+
+        //===================================================================
+        #region ACTION EFFECTUE SI UNE CARTE EST LUE 
+        private void MBmaster_OnResponseData(ushort ID, byte unit, byte function, byte[] values)
+        {
+            Dispatcher.Invoke((D_UpdateUI_tagRFID)UpdateUI, values);
+        }
+        #endregion
+
+        //===================================================================
+        #region ACTION EFFECTUE SI AUCUNE CARTE EST PRESENTE
+        private void MBmaster_OnException(ushort id, byte unit, byte function, byte exception)
+        {
+            Dispatcher.Invoke((D_UpdateUI_empty)UpdateUI);
+        }
+        #endregion
+
+        //===================================================================
+        #region DELEGATE DE MISE A JOUR UI
+        private void UpdateUI(byte[] values) { labelTAGRFID.Content = values[0] + " | " + values[1] + " | " + values[2] + " | " + values[3] + " | " + values[4] + " | " + values[5] + " | " + values[6] + " | " + values[7] + " | " + values[8] + " | " + values[9]; }
+        private void UpdateUI() { labelTAGRFID.Content = "- - -"; }
+        #endregion
+
+    }
+}
---
+++ AfficheurTagRFID/AfficheurTagRFID/Settings.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+using MahApps.Metro.Controls;
+
+namespace AfficheurTagRFID
+{
+    public partial class Settings : MetroWindow
+    {
+        //===============================================================
+        #region MAIN
+        public Settings()
+        {
+            InitializeComponent();
+            //===============================================================
+            #region INITIALISATION DES LABELS
+            labelUNIT.Text         = Properties.Settings.Default.Unit;
+            labelSTARTADDRESS.Text = Properties.Settings.Default.startAddress;
+            labelSIZE.Text         = Properties.Settings.Default.Size;
+            labelSTATIONRFID.Text  = Properties.Settings.Default.adresseIP;
+            #endregion
+        }
+        #endregion
+
+        //===============================================================
+        #region ACTION BOUTON VALIDER
+        private void Button_Valider(object sender, RoutedEventArgs e)
+        {
+            //===============================================================
+            #region ENREGISTREMENT DES VALEURS
+            Properties.Settings.Default["Unit"]         = labelUNIT.Text;
+            Properties.Settings.Default["startAddress"] = labelSTARTADDRESS.Text;
+            Properties.Settings.Default["Size"]         = labelSIZE.Text;
+            Properties.Settings.Default["adresseIP"]    = labelSTATIONRFID.Text;
+
+            Properties.Settings.Default.Save();
+            #endregion
+            this.Close();
+        }
+        #endregion
+
+    }
+}
